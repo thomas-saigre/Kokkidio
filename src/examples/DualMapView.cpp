a5ef1ab15d061a88c1c80c0c3387b39b18c3501a
lens <lennart.steffen@tu-berlin.de>
2024-08-07 17:43:16 +0200

    WIP: README and examples

---
+++ src/examples/DualMapView.cpp
@@ -0,0 +1,57 @@
+#include <Kokkidio.hpp>
+#include <iostream>
+#include <string_view>
+
+#include "magic_enum.hpp"
+
+
+int main(int argc, char** argv){
+	Kokkos::ScopeGuard guard(argc, argv);
+
+	using namespace Kokkidio;
+	std::cout << "DefaultTarget: " << magic_enum::enum_name(DefaultTarget) <<'\n';
+
+	int nRows {10}, nCols {20};
+
+	/* existing Eigen object */
+	Eigen::ArrayXXd eigenArray {nRows, nCols};
+
+	/*********************************************************/
+	/* Construction */
+	/*********************************************************/
+
+	/* By default, when initialising with an Eigen object,
+	 * the object's data is copied to the target. 
+	 * This behaviour be changed with an optional parameter: DontCopyToTarget */
+	DualMapView d1 {eigenArray};
+	auto d2 = dualMapView(eigenArray, DontCopyToTarget);
+	/* Otherwise, a DualMapView can be created in exactly the same ways as a 
+	 * MapView, so please refer to MapView.cpp for more examples. */
+
+
+	/*********************************************************/
+	/* Using DualMapView */
+	/*********************************************************/
+	/* with DualMapView, you can set your values on host, 
+	 * then copy them to the target: */
+	d2.map_host() = 123;
+	d2.copyToTarget();
+
+	auto print = [&](std::string_view descriptor){
+		std::cout
+			<< "d2, values on host, " << descriptor << ":\n"
+			<< d2.map_host() << '\n';
+	};
+	print("before");
+
+	/* Now you can do some computations on the target, 
+	 * then copy the values back */
+	parallel_for(d2.cols(), KOKKOS_LAMBDA(ParallelRange<> rng){
+		rng(d2) += 1;
+	});
+	d2.copyToHost();
+
+	print("after");
+
+	return 0;
+}
--- src/examples/MapView.cpp
+++ src/examples/MapView.cpp
@@ -1,23 +1,37 @@
 #include <Kokkidio.hpp>
+#include <iostream>
+
+#include "magic_enum.hpp"
 
 
 int main(int argc, char** argv){
 	Kokkos::ScopeGuard guard(argc, argv);
 
 	using namespace Kokkidio;
+	std::cout << "DefaultTarget: " << magic_enum::enum_name(DefaultTarget) <<'\n';
+
 	int nRows {10}, nCols {20};
 
 	/* existing Eigen object */
 	Eigen::ArrayXXd eigenArray {nRows, nCols};
 
-	/* Create MapView using a constructor or factory function.
-	 * Deduces Eigen type, and uses default target */
-	MapView mv1 {eigenArray};
-	auto mv2 = mapView(eigenArray);
+	/*********************************************************/
+	/* Construction */
+	/*********************************************************/
 
 	/* Create MapView using factory function for specific target,
 	 * while deducing Eigen type */
-	auto mv3 = mapView<Target::host>(eigenArray);
+	auto mv1 = mapView<Target::host>(eigenArray);
+
+	/* Create MapView using a constructor or factory function.
+	 * Deduces Eigen type, and uses default target */
+	MapView mv2 {eigenArray};
+	auto mv3 = mapView(eigenArray);
+	/* Note: 
+	 * Do not actually pass the same Eigen object to multiple MapViews!
+	 * If you do, then resize() will only apply to the Eigen object
+	 * and the MapView it was called on, but not the other MapViews. */
+
 
 	/* Create MapView using size parameters. 
 	 * ArrayXXd is dynamically sized in both dimensions, 
@@ -29,6 +43,25 @@ int main(int argc, char** argv){
 
 	/* Array3d is a fixed size type, so no parameters are required */
 	MapView<Eigen::Array3d> mv6;
+
+
+	/*********************************************************/
+	/* Using MapView */
+	/*********************************************************/
+
+	/* set values on host, using Eigen's assignment operator on MapView::map() */
+	mv1.map() = 1;
+	/* set values on target, using Kokkos::deep_copy with MapView::view() */
+	Kokkos::deep_copy(mv2.view(), 2);
+	/* set values on target with parallel dispatch: */
+	/* with Kokkidio::ParallelRange */
+	parallel_for( mv3.cols(), KOKKOS_LAMBDA(ParallelRange<> rng){
+		rng(mv3) = 3;
+	});
+	/* or just an integer, using the standard Kokkos-style */
+	parallel_for( mv4.size(), KOKKOS_LAMBDA(int i){
+		mv4.data()[i] = 4;
+	});
 
 	return 0;
 }
