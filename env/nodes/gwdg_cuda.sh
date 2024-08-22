7b562c07b8127a7477a624bf723a79c2b5cdc357
lens <lennart.steffen@posteo.de>
2024-08-22 12:09:52 +0200

    node files: refined gwdg_cuda.sh

--- env/nodes/gwdg_cuda.sh
+++ env/nodes/gwdg_cuda.sh
@@ -1,5 +1,9 @@
 
-module load cmake gcc cuda
+module load cuda
+# module load gcc/9.3.0
+module load gcc # is v. 12.3 at time of writing
+module load cmake/3.26.4
+
 backend_default=cuda
 
 PKGDIR="/scratch-emmy/usr/`whoami`"
@@ -7,3 +11,6 @@ source "${env_dir}/pkgdir_shared.sh"
 
 Kokkos_ARCH=Kokkos_ARCH_AMPERE80
 # Kokkos_ARCH=Kokkos_ARCH_HOPPER90
+
+export CC=`command -v gcc`
+export CXX=`command -v g++`
