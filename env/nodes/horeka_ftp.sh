efaecc496104701df630917d28677fccb319eee9
lens@tu-berlin.de <id_evb2673@ftp-x86n2.localdomain>
2024-08-23 20:01:51 +0200

    update node file for AMD MI100 machine

--- env/nodes/horeka_ftp.sh
+++ env/nodes/horeka_ftp.sh
@@ -9,10 +9,15 @@ Kokkos_ARCH=Kokkos_ARCH_AMD_GFX908
 # recommendations from 
 # https://www.nhr.kit.edu/userdocs/ftp/amd-rocm/
 module load toolkit/rocm 
-module load compiler/gnu
+# module load compiler/gnu
 module load devel/cmake
 
 ROCM_DIR="/opt/rocm-6.1.0"
 cmakeFlags_add="-DCMAKE_PREFIX_PATH=$ROCM_DIR"
 
-export CXX=`command -v hipcc`
+# export CXX=`command -v hipcc`
+export PATH=$(prepend_path "$ROCM_DIR/lib/llvm/bin" "${PATH:-}")
+
+# echo "which clang: " `which clang`
+export CXX=`command -v clang++`
+export CC=`command -v clang`
