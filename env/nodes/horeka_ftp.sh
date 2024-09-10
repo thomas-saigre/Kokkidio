f9c15f682ff28f8ee66f60625d6f0e8d1bb8d73d
lens <lens@tu-berlin.de>
2024-09-10 11:38:08 +0200

    expanded FTP node file to include cpu_gcc backend

--- env/nodes/horeka_ftp.sh
+++ env/nodes/horeka_ftp.sh
@@ -6,18 +6,30 @@ source "${env_dir}/pkgdir_shared.sh"
 # AMD Instinct MI100
 Kokkos_ARCH=Kokkos_ARCH_AMD_GFX908
 
-# recommendations from 
-# https://www.nhr.kit.edu/userdocs/ftp/amd-rocm/
-module load toolkit/rocm 
-# module load compiler/gnu
+
 module load devel/cmake
 
-ROCM_DIR="/opt/rocm-6.1.0"
-cmakeFlags_add="-DCMAKE_PREFIX_PATH=$ROCM_DIR"
+if [[ "$backend" == "cpu_gcc" ]]; then
+	# # doesn't work (2024-09-09): even clang --version causes "illegal instruction" error
+	# module load compiler/llvm
+	# instead: 
+	module load compiler/gnu
 
-# export CXX=`command -v hipcc`
-export PATH=$(prepend_path "$ROCM_DIR/lib/llvm/bin" "${PATH:-}")
+	echo "which g++: " `which g++`
+	export CXX=`command -v g++`
+	export CC=`command -v gcc`
+else
+	# recommendations from 
+	# https://www.nhr.kit.edu/userdocs/ftp/amd-rocm/
+	module load toolkit/rocm 
 
-# echo "which clang: " `which clang`
-export CXX=`command -v clang++`
-export CC=`command -v clang`
+	ROCM_DIR="/opt/rocm-6.1.0"
+	cmakeFlags_add="-DCMAKE_PREFIX_PATH=$ROCM_DIR"
+
+	# export CXX=`command -v hipcc`
+	export PATH=$(prepend_path "$ROCM_DIR/lib/llvm/bin" "${PATH:-}")
+
+	echo "which clang: " `which clang`
+	export CXX=`command -v clang++`
+	export CC=`command -v clang`
+fi
