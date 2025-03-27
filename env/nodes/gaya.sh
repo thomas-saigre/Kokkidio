5002a9fcec64bd733ad41f34b789d6e969c55780
Thomas Saigre <saigre@math.unistra.fr>
2025-03-27 10:32:57 +0100

    up gaya

---
+++ env/nodes/gaya.sh
@@ -0,0 +1,37 @@
+
+# uncomment one of these
+# backend_default=cuda
+# backend_default=hip
+# backend_default=sycl
+backend_default=ompt
+
+# see https://kokkos.org/kokkos-core-wiki/keywords.html#architectures
+# for possible values
+Kokkos_ARCH=Kokkos_ARCH_NATIVE
+
+# feel free to adjust this path to your liking
+Kokkos_BASE="./lib/kokkos"
+# only Kokkos_SRC is required
+Kokkos_SRC="$Kokkos_BASE/src/kokkos-dev"
+# Kokkos_BUILD and Kokkos_INST are optional,
+# but may be used to specify build/install directories for Kokkos.
+Kokkos_BUILD="$Kokkos_BASE/build"
+Kokkos_INST="$Kokkos_BASE/install"
+
+Eigen_BASE="./lib/eigen"
+# only Eigen_SRC is required
+Eigen_SRC="$Eigen_BASE"
+# As a pure header library, Eigen doesn't need to be compiled.
+# build and install directory can be the same, if you don't specifically wish
+# for the headers to be copied to standard directories.
+Eigen_BUILD="$Eigen_BASE/build"
+# Eigen_INST="$Eigen_BASE/install"
+
+
+download_opt=true
+buildKokkos=true
+buildEigen=true
+install_opt=true
+buildKokkidio=true
+buildTests=true
+buildExamples=true
---
+++ usage.md
@@ -0,0 +1,10 @@
+# Setup Kokkidio
+
+- Run `build.sh` to set-up the _node file_.
+- To download an build kokkos and eige, add the following lines to the `build.sh` file:
+```bash
+download_opt=true
+buildKokkos=true
+buildEigen=true
+```
+and run `build.sh` again.
